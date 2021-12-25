6e6b2760c3f5cdc60c8479fd13387008d96d9d98
nadiaH <nadiahrb79@gmailcom>
2021-12-25 14:25:27 +0200

    f out

--- src/plugin.js
+++ src/plugin.js
@@ -1,30 +1,34 @@
 'use strict';
+
 const plugin = document.getElementById('idward-plugin');
 
 if (plugin.getAttribute('isRunning') === 'true') {
   createButton();
-
-  function createButton() {
-    const button = document.createElement('button');
-    button.onclick = openModal;
-    button.style.cssText = `
-      border: none;
-      background-color: transparent;
-      border-left: 100px solid #0645ad;
-      border-top: 100px solid transparent;
-      position: fixed;
-      bottom: 0;
-      left: 0;
-      cursor: pointer;
-      z-index: 5;
-    `;
-
-    document.body.appendChild(button);
-  }
 }
 
 function openModal() {
-  const dialog = document.createElement('div');
+  // const dialog = document.createElement('div');
+  console.log('Hello');
+}
+
+function createButton() {
+  const button = document.createElement('button');
+
+  button.onclick = openModal;
+
+  button.style.cssText = `
+    border: none;
+    background-color: transparent;
+    border-left: 100px solid #0645ad;
+    border-top: 100px solid transparent;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    cursor: pointer;
+    z-index: 5;
+  `;
+
+  document.body.appendChild(button);
 }
 
 // const script = document.createElement('script');
@@ -32,4 +36,3 @@ function openModal() {
 // script.src = 'collect.js';
 
 // document.head.appendChild(script);
-
