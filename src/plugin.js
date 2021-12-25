042c3a189628ffbe796d04df7a4f71cd69e2ef9a
nadiaH <nadiahrb79@gmailcom>
2021-12-25 14:21:12 +0200

    z-index

--- src/plugin.js
+++ src/plugin.js
@@ -1,20 +1,35 @@
 'use strict';
+const plugin = document.getElementById('idward-plugin');
 
-createButton();
+if (plugin.getAttribute('isRunning') === 'true') {
+  createButton();
 
-function createButton() {
-  let button = document.createElement('button');
-  button.style.cssText = `
-    border: none;
-    background-color: transparent;
-    border-left: 120px solid #000;
-    border-top: 120px solid transparent;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    cursor: pointer;
-  `;
+  function createButton() {
+    const button = document.createElement('button');
+    button.onclick = openModal;
+    button.style.cssText = `
+      border: none;
+      background-color: transparent;
+      border-left: 100px solid #0645ad;
+      border-top: 100px solid transparent;
+      position: fixed;
+      bottom: 0;
+      left: 0;
+      cursor: pointer;
+      z-index: 5;
+    `;
 
-  document.body.appendChild(button);
+    document.body.appendChild(button);
+  }
 }
+
+function openModal() {
+  const dialog = document.createElement('div');
+}
+
+// const script = document.createElement('script');
+// script.type = 'text/javascript';
+// script.src = 'collect.js';
+
+// document.head.appendChild(script);
 
