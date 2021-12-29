1d05a146e42b20c97b201e98f5a9c57ba8288e46
nadiaH <nadiahrb79@gmailcom>
2021-12-29 14:05:50 +0200

    backdrop

--- src/plugin.js
+++ src/plugin.js
@@ -1,52 +1,105 @@
 'use strict';
 
 const plugin = document.getElementById('idward-plugin');
+let freezeClic = false;
 
 if (plugin.getAttribute('isRunning') === 'true') {
   createButton();
+  createModal();
 }
 
 function reject() {
   reloadScrollBars();
   console.log('reject');
-  document.getElementById('id01').style.display='none';
+  document.getElementById('mod01').style.visibility = 'hidden';
+  document.getElementById('bckd').style.visibility = 'hidden';
 }
 
 function accept() {
+  // const script = document.createElement('script');
+  // script.type = 'text/javascript';
+  // script.src = './collect.js';
+  // script.setAttribute('src', 'collect.js')
+
+  // document.body.appendChild(script);
+  // console.log(script);
   reloadScrollBars();
   console.log('accept');
-  document.getElementById('id01').style.display='none';
+  document.getElementById('mod01').style.visibility = 'hidden';
+  document.getElementById('bckd').style.visibility = 'hidden';
 }
 
 function reloadScrollBars() {
   document.documentElement.style.overflow = 'auto';
+  document.getElementById('mySidebar').style.overflow = 'auto';
+  document.getElementById('mySidebar').style.pointerEvents = 'auto';
+  freezeClic = !freezeClic;
 }
 
 function unloadScrollBars() {
   document.documentElement.style.overflow = 'hidden';
+  document.getElementById('mySidebar').style.overflow = 'hidden';
+  document.getElementById('mySidebar').style.pointerEvents = 'none';
+  freezeClic = !freezeClic;
 }
 
 function openModal() {
-  const dialog = document.createElement('div');
-  dialog.classList.add('w3-modal');
-  dialog.id = 'id01';
-  dialog.style.cssText = `display: block;`;
-  dialog.innerHTML = `
-    <div class="w3-modal-content">
-      <div class="w3-container">
-        <div class="w3-card" style="
-          padding: 30px;
-          display: flex;
-          justify-content: space-around;
-        ">
-          <button class="w3-button w3-teal" id="acc">Accept</button>
-          <button class="w3-button w3-red" id="rej">Reject</button>
-        </div>
-      </div>
-    </div>`;
-
-  document.body.appendChild(dialog);
+  document.getElementById('bckd').style.visibility = 'visible';
+  document.getElementById('mod01').style.visibility = 'visible';
   unloadScrollBars();
+}
+
+function createModal() {
+  const backdrop = document.createElement('div');
+  backdrop.id = 'bckd';
+  backdrop.style.cssText = `
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.5);
+    z-index: 5;
+    visibility: hidden;
+  `;
+  const dialog = document.createElement('div');
+  dialog.classList.add('modal');
+  dialog.id = 'mod01';
+  dialog.innerHTML = `
+    <button class="w3-button w3-teal" id="acc">Accept</button>
+    <button class="w3-button w3-red" id="rej">Reject</button>
+  `;
+  dialog.style.cssText = `
+    visibility: hidden;
+    position: fixed;
+    position: absolut;
+    z-index: 6;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    width: 500px;
+    heigth: 200px;
+    overflow: auto;
+    padding: 30px;
+    display: flex;
+    justify-content: space-around;
+    background-color: #ffffff;
+    border: 1px solid #999;
+    border: 1px solid rgba(0, 0, 0, 0.3);
+    *border: 1px solid #999;
+    -webkit-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+    box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+    -webkit-background-clip: padding-box;
+    -moz-background-clip: padding-box;
+    background-clip: padding-box;
+  `;
+
+  document.body.appendChild(backdrop);
+  backdrop.appendChild(dialog);
+
 
   const rejBtn = document.getElementById('rej');
   rejBtn.addEventListener('click', reject);
@@ -74,9 +127,3 @@ function createButton() {
 
   document.body.appendChild(triangleBtn);
 }
-
-// const script = document.createElement('script');
-// script.type = 'text/javascript';
-// script.src = 'collect.js';
-
-// document.head.appendChild(script);
